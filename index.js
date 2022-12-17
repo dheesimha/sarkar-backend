32ef75123d64877122e114c9f8f6912bf28cfe7a
Dheemanth Narasimha <dheemanth.simha01@gmail.com>
2022-12-18 00:12:39 +0530

    Abstract port

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.get("/", function (req, res) {
 
 
 
-app.listen(3000, function () {
-    console.log("Server is running on port 3000");
+app.listen(process.env.PORT, function () {
+    console.log("Server is running");
 })
 
 // db
